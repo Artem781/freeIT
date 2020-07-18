849c6e5df0475a071970bbb3412d045f8fee1e45
Artem Naron <naronartem@gmail.com>
2020-07-18 23:16:13 +0300

    lesson2 finished

--- src/main/java/com/freeit/anaron/lesson2/Task6.java
+++
@@ -1,7 +0,0 @@
-package com.freeit.anaron.lesson2;
-//6) Даны 2 числа. Вывести большее из них
-public class Task6 {
-    public int findLargerNumber(int a, int b){
-        return Math.max(a, b);
-    }
-}
